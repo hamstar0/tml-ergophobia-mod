97b9d37fbe986af245fd4077b441a6259709296d
hamstar0 <webutek@gmail.com>
2021-03-12 04:29:53 -0800

    v1.6.1 - Restructured player tile placement code files

--- Ergophobia/MyPlayer_TilePlacement_Alerts.cs
+++ Ergophobia/MyPlayer_TilePlacement_Alerts.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 using HamstarHelpers.Helpers.Debug;
-using HamstarHelpers.Helpers.Players;
 using Ergophobia.Items.HouseFramingKit;
 using Ergophobia.Items.HouseFurnishingKit;
 using Ergophobia.Items.ScaffoldingKit;
-using Ergophobia.Items.TrackDeploymentKit;
-using Ergophobia.Protocols;
 
 
 namespace Ergophobia {
@@ -55,6 +51,7 @@ namespace Ergophobia {
 				this._HasPreviousHouseViableAlert = false;
 			}
 		}
+
 
 		private void CheckFrameableHouse() {
 			int tileX = (int)this.player.Center.X >> 4;
@@ -103,36 +100,6 @@ namespace Ergophobia {
 					dust.noGravity = true;
 					dust.noLight = true;
 				}
-			}
-		}
-
-
-		private void CheckTrackKitResume( int heldTrackKitItemType ) {
-			if( !this.player.mount.Active || !this.player.mount.Cart ) {
-				return;
-			}
-
-			var trackKitSingleton = ModContent.GetInstance<TrackDeploymentKitItem>();
-			(int x, int y, int dir) resume = trackKitSingleton.ResumeDeploymentAt;
-			var resumeWldPos = new Vector2( (resume.x << 4) + 8, (resume.y << 4) + 8 );
-
-			if( Vector2.DistanceSquared(this.player.Center, resumeWldPos) >= 4096 ) { // 4 tiles
-				return;
-			}
-
-			PlayerItemHelpers.RemoveInventoryItemQuantity( this.player, heldTrackKitItemType, 1 );
-
-			int leftovers = TrackDeploymentKitItem.Redeploy( this.player.whoAmI );
-			if( leftovers == 0 ) {
-				return;
-			}
-
-			int itemWho = Item.NewItem( resumeWldPos, ItemID.MinecartTrack, leftovers );
-
-			if( Main.netMode != NetmodeID.MultiplayerClient ) {
-				NetMessage.SendData( MessageID.SyncItem, -1, -1, null, itemWho, 1f );
-			} else {
-				TrackKitDeployProtocol.SendToServer( resume.dir > 0, resume.x, resume.y, true );
 			}
 		}
 	}
---
+++ Ergophobia/MyPlayer_TilePlacement_TracksResume.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using HamstarHelpers.Helpers.Debug;
+using HamstarHelpers.Helpers.Players;
+using Ergophobia.Items.TrackDeploymentKit;
+using Ergophobia.Protocols;
+
+
+namespace Ergophobia {
+	partial class ErgophobiaPlayer : ModPlayer {
+		private void CheckTrackKitResume( int heldTrackKitItemType ) {
+			if( !this.player.mount.Active || !this.player.mount.Cart ) {
+				return;
+			}
+
+			var trackKitSingleton = ModContent.GetInstance<TrackDeploymentKitItem>();
+			(int x, int y, int dir) resume = trackKitSingleton.ResumeDeploymentAt;
+			var resumeWldPos = new Vector2( (resume.x << 4) + 8, (resume.y << 4) + 8 );
+
+			if( Vector2.DistanceSquared(this.player.Center, resumeWldPos) >= 4096 ) { // 4 tiles
+				return;
+			}
+
+			PlayerItemHelpers.RemoveInventoryItemQuantity( this.player, heldTrackKitItemType, 1 );
+
+			int leftovers = TrackDeploymentKitItem.Redeploy( this.player.whoAmI );
+			if( leftovers == 0 ) {
+				return;
+			}
+
+			int itemWho = Item.NewItem( resumeWldPos, ItemID.MinecartTrack, leftovers );
+
+			if( Main.netMode != NetmodeID.MultiplayerClient ) {
+				NetMessage.SendData( MessageID.SyncItem, -1, -1, null, itemWho, 1f );
+			} else {
+				TrackKitDeployProtocol.SendToServer( resume.dir > 0, resume.x, resume.y, true );
+			}
+		}
+	}
+}
